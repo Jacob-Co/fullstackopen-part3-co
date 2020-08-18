4e503b59effb6846d9ca80477a6a744b4c9d3ef9
Jacobtco <jacobtco@gmail.com>
2020-08-18 16:01:04 +0800

    Added cors module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 let persons = [
   {
@@ -42,6 +43,7 @@ app.use(express.json());
 // morgan.token('type', function (req, res) { return req.headers['content-type'] })
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
