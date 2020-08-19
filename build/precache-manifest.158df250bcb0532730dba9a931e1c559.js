42e4e216e5dc6dfa6295e6ae72a7b51f8d934787
Jacobtco <jacobtco@gmail.com>
2020-08-19 18:17:36 +0800

    Added new prod build

---
+++ build/precache-manifest.158df250bcb0532730dba9a931e1c559.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba28a5a8b2ac7b99a51b803b83ced37c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c331553a81a5fd068b46",
+    "url": "/static/js/2.a4895e77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4895e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1f1eb91c6b1ddafe38",
+    "url": "/static/js/main.16f25e05.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
